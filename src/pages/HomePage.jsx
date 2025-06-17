0627a8a8f6366e3c007e66c5468d0b15662f3e3e
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-17 17:18:01 -0500

    feat(ui): mostrar lista de cursos desde la API en HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
+import courseService from "../api/courseService";
+import { useEffect, useState } from "react";
 
 function HomePage() {
+  const [courses, seetCourses] = useState([]);
+
+  const fetchCourses = async () => {
+    const data = await courseService.getAll();
+    seetCourses(data)
+  }
+
+  useEffect(() => {
+    fetchCourses()
+  }, []);
   return (
     <>
       <Header />
@@ -9,7 +21,18 @@ function HomePage() {
         <div className="row g-0 flex-grow-1">
             <SideBar className="col-1 d-flex" />
             <main className="col bg-light">
-                Main
+                <h2>Cursos</h2>
+            {courses.length === 0 ? (
+              <p>No hay cursos disponibles.</p>
+            ) : (
+              <ul className="list-group">
+                {courses.map((course) => (
+                  <li key={course.id} className="list-group-item">
+                    ID: {course.id} - Materia: {course.subject?.name || "Sin nombre"} - Profesor: {course.professor?.username || "Sin nombre"}
+                  </li>
+                ))}
+              </ul>
+            )}
             </main>
         </div>
       </div>
