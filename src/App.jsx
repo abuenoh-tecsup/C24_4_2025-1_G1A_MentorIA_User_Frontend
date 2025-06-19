99717a065721eda7f30bcf75084ac1bc9b203e4c
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-19 01:06:34 -0500

    feat(router): agregar rutas para Material, Task, Evaluation y Forum

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import HomePage from './pages/HomePage'
 import CoursePage from './pages/CoursePage'
 import ModulePage from './pages/ModulePage'
 import ModuleFormPage from './pages/ModuleFormPage'
+import MaterialPage from './pages/MaterialPage'
+import TaskPage from './pages/TaskPage'
+import EvaluationPage from './pages/EvaluationPage'
+import ForumPage from './pages/ForumPage'
 
 
 function App() {
@@ -12,8 +16,14 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route path="home" element={<HomePage/>}/>
+
         <Route path="courses/:courseId" element={<CoursePage/>}/>
         <Route path="courses/:courseId/modules" element={<ModulePage/>}/>
+        <Route path="courses/:courseId/materials" element={<MaterialPage/>}/>
+        <Route path="courses/:courseId/tasks" element={<TaskPage/>}/>
+        <Route path="courses/:courseId/evaluations" element={<EvaluationPage/>}/>
+        <Route path="courses/:courseId/forums" element={<ForumPage/>}/>
+
         <Route path="/courses/:courseId/modules/form" element={<ModuleFormPage />} />
       </Route>
     </Routes>
