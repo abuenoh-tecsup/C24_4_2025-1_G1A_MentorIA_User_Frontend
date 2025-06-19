d1622f8d61bdd12d6c890a68982f7e2a7645ecdb
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-19 04:34:49 -0500

    feat(router): agregar rutas para páginas de detalle (material, task, evaluation, forum)

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,60 @@
-import { Route, Routes } from 'react-router-dom'
-import Layout from './pages/Layout'
-import HomePage from './pages/HomePage'
-import CoursePage from './pages/CoursePage'
-import ModulePage from './pages/ModulePage'
-import ModuleFormPage from './pages/ModuleFormPage'
-import MaterialPage from './pages/MaterialPage'
-import TaskPage from './pages/TaskPage'
-import EvaluationPage from './pages/EvaluationPage'
-import ForumPage from './pages/ForumPage'
-import MaterialFormPage from './pages/MaterialFormPage'
-import EvaluationFormPage from './pages/EvaluationFormPage'
-import ForumFormPage from './pages/ForumFormPage'
-import TaskFormPage from './pages/TaskFormPage'
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import CoursePage from "./pages/CoursePage";
+import ModulePage from "./pages/ModulePage";
+import ModuleFormPage from "./pages/ModuleFormPage";
+import MaterialPage from "./pages/MaterialPage";
+import TaskPage from "./pages/TaskPage";
+import EvaluationPage from "./pages/EvaluationPage";
+import ForumPage from "./pages/ForumPage";
+import MaterialFormPage from "./pages/MaterialFormPage";
+import EvaluationFormPage from "./pages/EvaluationFormPage";
+import ForumFormPage from "./pages/ForumFormPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import MaterialDetailPage from "./pages/MaterialDetailPage";
+import EvaluationDetailPage from "./pages/EvaluationDetailPage";
+import ForumDetailPage from "./pages/ForumDetailPage";
+import TaskDetailPage from "./pages/TaskDetailPage";
 
 function App() {
-
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route path="home" element={<HomePage/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route path="home" element={<HomePage />} />
+        <Route path="courses/:courseId" element={<CoursePage />} />
 
-        <Route path="courses/:courseId" element={<CoursePage/>}/>
-        <Route path="courses/:courseId/modules" element={<ModulePage/>}/>
-        <Route path="courses/:courseId/materials" element={<MaterialPage/>}/>
-        <Route path="courses/:courseId/tasks" element={<TaskPage/>}/>
-        <Route path="courses/:courseId/evaluations" element={<EvaluationPage/>}/>
-        <Route path="courses/:courseId/forums" element={<ForumPage/>}/>
+        <Route path="courses/:courseId/modules">
+          <Route index element={<ModulePage />} />
+          <Route path="form" element={<ModuleFormPage />} />
+        </Route>
 
-        <Route path="/courses/:courseId/modules/form" element={<ModuleFormPage />} />
-        <Route path="/courses/:courseId/materials/form" element={<MaterialFormPage/>} />
-        <Route path="/courses/:courseId/evaluations/form" element={<EvaluationFormPage/>} />
-        <Route path="/courses/:courseId/forums/form" element={<ForumFormPage/>} />
-        <Route path="/courses/:courseId/tasks/form" element={<TaskFormPage/>} />
+        <Route path="courses/:courseId/materials">
+          <Route index element={<MaterialPage />} />
+          <Route path="form" element={<MaterialFormPage />} />
+          <Route path=":materialId" element={<MaterialDetailPage />} />
+        </Route>
+
+        <Route path="courses/:courseId/tasks">
+          <Route index element={<TaskPage />} />
+          <Route path="form" element={<TaskFormPage />} />
+          <Route path=":taskId" element={<TaskDetailPage />} />
+        </Route>
+
+        <Route path="courses/:courseId/evaluations">
+          <Route index element={<EvaluationPage />} />
+          <Route path="form" element={<EvaluationFormPage />} />
+          <Route path=":evaluationId" element={<EvaluationDetailPage />} />
+        </Route>
+
+        <Route path="courses/:courseId/forums">
+          <Route index element={<ForumPage />} />
+          <Route path="form" element={<ForumFormPage />} />
+          <Route path=":forumId" element={<ForumDetailPage />} />
+        </Route>
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
